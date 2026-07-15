e745e96858d19ac3b9d7a8131912401efcdb40fb
Tejaa24 <adilakshammabonam@gaill.com>
2026-07-15 22:03:21 +0530

    Added model evaluation and project improvements

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request
 import joblib
 import numpy as np
 
+
 app = Flask(__name__)
 
-# Load trained model and preprocessing files
+
+# Load ML files
 model = joblib.load("models/crop_model.pkl")
 scaler = joblib.load("models/scaler.pkl")
 label_encoder = joblib.load("models/label_encoder.pkl")
@@ -18,23 +20,35 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
 
-    N = float(request.form["N"])
-    P = float(request.form["P"])
-    K = float(request.form["K"])
-    temperature = float(request.form["temperature"])
-    humidity = float(request.form["humidity"])
-    ph = float(request.form["ph"])
-    rainfall = float(request.form["rainfall"])
+    try:
+        N = float(request.form["N"])
+        P = float(request.form["P"])
+        K = float(request.form["K"])
+        temperature = float(request.form["temperature"])
+        humidity = float(request.form["humidity"])
+        ph = float(request.form["ph"])
+        rainfall = float(request.form["rainfall"])
 
-    data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+        input_data = np.array(
+            [[N, P, K, temperature, humidity, ph, rainfall]]
+        )
 
-    data = scaler.transform(data)
+        input_scaled = scaler.transform(input_data)
 
-    prediction = model.predict(data)
+        prediction = model.predict(input_scaled)
 
-    crop = label_encoder.inverse_transform(prediction)
+        crop = label_encoder.inverse_transform(prediction)[0]
 
-    return render_template("result.html", prediction=crop[0])
+        return render_template(
+            "result.html",
+            prediction=crop
+        )
+
+    except Exception as e:
+        return render_template(
+            "error.html",
+            error=e
+        )
 
 
 if __name__ == "__main__":
